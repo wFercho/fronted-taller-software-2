565e62af2621461c29ce951dcaf6ec01b9d520c0
Fernando Salamanca <fesaba21@gmail.com>
2023-11-21 16:27:30 -0500

    feat: main view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+"use client"
+import { ChangeEvent, FormEvent, useState } from 'react'
 
+
+
+interface API_RESPONSE {
+  contador: number
+  frecuencias: Object
+}
+
+const RUTA_POST = 'http://localhost:4000/api/v1/normalizar_texto'
 export default function Home() {
+  const [texto, setTexto] = useState("")
+  const [files, setFiles] = useState<FileList | null>(null)
+  const [apiResponse, setApiResponse] = useState<API_RESPONSE | null>(null)
+  const [lista, setLista] = useState<any[] | null>(null)
+
+  const objetoToLista = ({ frecuencias }: API_RESPONSE) => {
+    return Object.entries(frecuencias);
+  }
+
+
+  const handleSubmitText = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (texto.length > 0) {
+      const formData = new FormData()
+      formData.append('texto', texto)
+      const response = await fetch(RUTA_POST, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: texto
+      })
+
+      const data = await response.json() as API_RESPONSE
+      setApiResponse(data)
+      const _lista = objetoToLista(data)
+      setLista(_lista)
+
+
+
+    }
+
+  }
+
+  const handleSubmitFile = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (files) {
+      const formData = new FormData()
+      formData.append('file', files[0])
+      const response = await fetch(RUTA_POST, {
+        method: 'POST',
+        body: formData
+      })
+
+      const data = await response.json() as API_RESPONSE
+      setApiResponse(data)
+      const _lista = objetoToLista(data)
+      setLista(_lista)
+      return
+    }
+
+    console.log("NO HAY ARCHIVOS SELECCIONADOS");
+
+
+  }
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const myFiles = e.target.files
+    if (myFiles) {
+      setFiles(myFiles)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex bg-gray-600 text-white h-full">
+      <section className='flex flex-col justify-around h-full items-center w-1/2 bg-sky-950'>
+        <form onSubmit={handleSubmitText} className='w-full'>
+          <section className='flex flex-col items-center'>
+            <h3 className='text-2xl mb-8'>TEXTO</h3>
+            <div className='mb-3 w-3/4'>
+              <label htmlFor="" className='block mb-2 text-sm font-medium dark:text-white'>Escriba un texto...</label>
+              <textarea rows={4} className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setTexto(e.currentTarget.value)}
+              ></textarea>
+            </div>
+            <button className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>ENVIAR</button>
+          </section>
+        </form>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <form onSubmit={handleSubmitFile} className='w-full'>
+          <section className='flex flex-col items-center'>
+            <h3 className='text-2xl mb-8'>ARCHIVO</h3>
+            <label htmlFor="input-upload-document" className='w-3/4 block mb-2 text-sm font-mediu dark:text-white'>
+              Subir archivo
+              <input type="file" id='input-upload-document'
+                accept='.txt'
+                onChange={handleFileChange}
+                className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400' />
+            </label>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <button className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>SUBIR</button>
+          </section>
+        </form>
+      </section>
+      <section className='w-1/2 bg-teal-600 overflow-y-scroll flex flex-col items-center'>
+        {apiResponse !== null ? <h2 className='text-xl font-bold'>Contador de palabras:{apiResponse.contador}</h2>: ""}
+        <ul>
+          {lista !== null ? lista.map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          )) : <h2>Acá se verá su texto...</h2>}
+        </ul>
+      </section>
     </main>
   )
 }
